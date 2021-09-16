cb5c37fbb8ccb3d8e59215ad2fa234aac6a41561
Qijie Xue <44390740+QijieXue@users.noreply.github.com>
2021-09-16 21:43:20 +0800

    Added App Center

--- xf.demo01/App.xaml.cs
+++ xf.demo01/App.xaml.cs
@@ -4,6 +4,11 @@ using Xamarin.Forms.Xaml;
 using xf.demo01.Services;
 using xf.demo01.Views;
 
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace xf.demo01
 {
     public partial class App : Application
@@ -19,6 +24,10 @@ namespace xf.demo01
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=2ea114b8-c51a-4f38-9171-54e04fcd063b;" +
+                  "ios=6a6edff1-af5c-45a8-a92b-45a4fcd8b87f;" +
+                  "uwp={Your UWP App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
