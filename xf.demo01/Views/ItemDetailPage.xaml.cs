ed8298399cce4b3e7c003af00bd61e0123f2406c
Qijie Xue <44390740+QijieXue@users.noreply.github.com>
2021-09-16 21:32:27 +0800

    添加项目文件。

---
+++ xf.demo01/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using xf.demo01.ViewModels;
+
+namespace xf.demo01.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ xf.demo01/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using xf.demo01.Models;
+using xf.demo01.ViewModels;
+
+namespace xf.demo01.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
