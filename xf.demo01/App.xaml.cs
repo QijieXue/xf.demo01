0e8fef1f2768ca69633ceea68eb2c267b50a1c2e
Qijie Xue <44390740+QijieXue@users.noreply.github.com>
2021-09-19 20:54:05 +0800

    Updated AppCenter

--- xf.demo01/App.xaml.cs
+++ xf.demo01/App.xaml.cs
@@ -8,6 +8,7 @@ using xf.demo01.Views;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
 
 namespace xf.demo01
 {
@@ -22,12 +23,19 @@ namespace xf.demo01
             MainPage = new AppShell();
         }
 
-        protected override void OnStart()
+        protected async override void OnStart()
         {
+            AppCenter.LogLevel = LogLevel.Verbose;
             AppCenter.Start("android=2ea114b8-c51a-4f38-9171-54e04fcd063b;" +
                   "ios=6a6edff1-af5c-45a8-a92b-45a4fcd8b87f;" +
                   "uwp={Your UWP App secret here}",
                   typeof(Analytics), typeof(Crashes));
+
+            var IsAnalyticsEnabled = await Analytics.IsEnabledAsync();
+
+            var IsCrashesEnabled = await Crashes.IsEnabledAsync();
+
+            var IsDistributionEnabled = await Distribute.IsEnabledAsync();
         }
 
         protected override void OnSleep()
